ee677faadd5c377f17344e9055fc87403570932d
ercoppa <ercoppa@gmail.com>
2025-05-25 23:21:51 +0200

    Update fix-absolute-img-url.py

--- scripts/fix-absolute-img-url.py
+++ scripts/fix-absolute-img-url.py
@@ -6,6 +6,6 @@ for filename in glob.glob('docs/_sources/src/*/*.ipynb'):
     print("Processing", filename)
     data = open(filename).read()
     d = os.path.basename(os.path.dirname(filename))
-    data = data.replace('(img/', '(https://introcp.github.io/' + d + '/img/')
-    data = data.replace('"img/', '"https://introcp.github.io/' + d + '/img/')
+    data = data.replace('(img/', '(https://introcp.github.io/src/' + d + '/img/')
+    data = data.replace('"img/', '"https://introcp.github.io/src/' + d + '/img/')
     open(filename, 'w').write(data)
